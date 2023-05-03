e5a0c7a391e2ce41bc9afd5bdb863be7d30b5123
unknown <filipov0991>
2023-05-03 11:25:22 +0500

    fix validation formula when adding equipment

--- drfsite/validators.py
+++ drfsite/validators.py
@@ -10,11 +10,11 @@ def validate_serial_number(serial_number, equipment_type_id):
         print(char, serial_number[i])
         if char == 'N' and not serial_number[i].isdigit():
             raise serializers.ValidationError('Символ {} в позиции {} не является цифрой'.format(serial_number[i], i))
-        elif char == 'A' and not serial_number[i].isalpha():
+        elif char == "A" and not serial_number[i].isalpha() and not serial_number[i].isupper():
             raise serializers.ValidationError('Символ {} в позиции {} не является буквой'.format(serial_number[i], i))
-        elif char == 'a' and not serial_number[i].isalpha():
+        elif char == "a" and not serial_number[i].isalpha() and not serial_number[i].islower():
             raise serializers.ValidationError('Символ {} в позиции {} не является буквой'.format(serial_number[i], i))
-        elif char == 'X' and not (serial_number[i].isalpha() or serial_number[i].isdigit()):
+        elif char == "X" and not (serial_number[i].isalpha() and serial_number[i].isupper()) and not serial_number[i].isdigit():
             raise serializers.ValidationError('Символ {} в позиции {} не является буквой или цифрой'.format(serial_number[i], i))
         elif char == 'Z' and serial_number[i] not in ['-', '_', '@']:
             raise serializers.ValidationError('Символ {} в позиции {} не является допустимым символом'.format(serial_number[i], i))
